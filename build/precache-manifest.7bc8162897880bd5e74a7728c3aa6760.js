5e0f9bdac2bce0218b1985fce89a162ce6c8b383
Alex <ascottjacobsen@gmail.com>
2020-06-27 11:14:29 -0500

    add build folder

---
+++ build/precache-manifest.7bc8162897880bd5e74a7728c3aa6760.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9d3cad0f20b7a260d100a4a8133b91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2c379a97e0eb37a75f8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "26e86a5bb00fbf8cb812",
+    "url": "/static/js/2.fa65a144.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa65a144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c379a97e0eb37a75f8",
+    "url": "/static/js/main.b6ab1d73.chunk.js"
+  },
+  {
+    "revision": "9137d8bf9219a67f3f57",
+    "url": "/static/js/runtime-main.0ac27353.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
