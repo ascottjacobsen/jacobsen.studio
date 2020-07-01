ed216d6401c4bf978b0b4529a0b66e70f12a5d09
Alex <ascottjacobsen@gmail.com>
2020-07-01 14:51:06 -0500

    add material buttons to nav bar

--- src/gem-serve-page/components/NavTray.js
+++ src/gem-serve-page/components/NavTray.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { FaGlobeEurope, FaCity, FaSearch, FaDotCircle } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import { Button } from "@material-ui/core";
+import NavButton from "./NavButton";
 
 class NavTray extends Component {
   render() {
@@ -16,65 +17,53 @@ class NavTray extends Component {
 
     return (
       <div className="nav-tray">
-        <Button
-          className={`btn ${
+        <NavButton
+          buttonClassName={`btn ${
             loadOpportunitiesCountriesOrSpheres === "countries" && !search
               ? "active"
               : ""
           }`}
-          value="countries"
-          onClick={(e) => switchToSpheresOrCountries("countries")}
+          buttonValue="countries"
+          buttonFunction={(e) => switchToSpheresOrCountries("countries")}
         >
+          {" "}
           <IconContext.Provider
             value={{ size: "1.5em", attr: { pointerEvents: "none" } }}
           >
             <FaGlobeEurope />
           </IconContext.Provider>
-        </Button>
-        <button
-          className={`btn${
+        </NavButton>
+
+        <NavButton
+          buttonClassName={`btn ${
             loadOpportunitiesCountriesOrSpheres === "spheres" && !search
-              ? " active"
+              ? "active"
               : ""
           }`}
-          value="spheres"
-          onClick={(e) => switchToSpheresOrCountries("spheres")}
+          buttonValue="spheres"
+          buttonFunction={(e) => switchToSpheresOrCountries("spheres")}
+        >
+          {" "}
+          <IconContext.Provider
+            value={{ size: "1.5em", attr: { pointerEvents: "none" } }}
+          >
+            <FaDotCircle />
+          </IconContext.Provider>
+        </NavButton>
+
+        <NavButton
+          buttonClassName={`btn ${search ? "active" : ""}`}
+          buttonValue="search"
+          buttonFunction={() => searchOn(true)}
         >
           {" "}
           <IconContext.Provider
             value={{ size: "1.5em", attr: { pointerEvents: "none" } }}
           >
             {" "}
-            <FaDotCircle />
-          </IconContext.Provider>
-        </button>
-        <button
-          className={`btn ${
-            focusCitiesVisible &&
-            loadOpportunitiesCountriesOrSpheres === "countries"
-              ? "active"
-              : ""
-          }`}
-          onClick={showFocusCities}
-        >
-          <IconContext.Provider
-            value={{ size: "1.5em", attr: { pointerEvents: "none" } }}
-          >
-            {" "}
-            <FaCity />
-          </IconContext.Provider>
-        </button>
-        <button
-          className={`btn ${search ? "active" : ""}`}
-          onClick={() => searchOn(true)}
-        >
-          <IconContext.Provider
-            value={{ size: "1.5em", attr: { pointerEvents: "none" } }}
-          >
-            {" "}
             <FaSearch />
           </IconContext.Provider>
-        </button>
+        </NavButton>
       </div>
     );
   }
