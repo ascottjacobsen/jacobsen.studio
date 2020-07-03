7b7caec39b36cf18520127d818208c4964886fcd
Alex <ascottjacobsen@gmail.com>
2020-07-03 09:19:17 -0500

    switch close icon to material, modularize styles

--- src/gem-serve-page/components/ContainerCloser.js
+++ src/gem-serve-page/components/ContainerCloser.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { FaTimes } from "react-icons/fa";
+import { ButtonBase } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import { makeStyles } from "@material-ui/core/styles";
+import { pink } from "@material-ui/core/colors";
 
-const ContainerCloser = props => {
+const useStyles = makeStyles({
+  closeRow: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: "10px 0",
+  },
+  closeButton: {
+    color: "#333333",
+    borderRadius: "100%",
+    width: 24,
+  },
+});
+
+const ContainerCloser = (props) => {
   const { closeFunctions } = props;
+  const classes = useStyles();
   return (
-    <div className="serveOp serveOp-close-container-row">
-      <button
-        className="serveOp serveOp-close-button"
-        onClick={e => {
+    <div className={classes.closeRow}>
+      <ButtonBase
+        className={classes.closeButton}
+        onClick={(e) => {
           closeFunctions();
         }}
       >
-        <FaTimes />
-      </button>
+        <CloseIcon />
+      </ButtonBase>
     </div>
   );
 };
