6e87125d5aa48a04160ee49e030b0c0d04c9e49f
Alex <ascottjacobsen@gmail.com>
2020-07-10 16:23:53 -0500

    remove react warnings from previous commit

--- src/gem-serve-page/components/NavButton.js
+++ src/gem-serve-page/components/NavButton.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import { Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { IconContext } from "react-icons";
